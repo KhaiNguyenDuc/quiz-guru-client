c96e7f3bc5136af5c7fa13f598e569f0ff516b7e
KhaiNguyenDuc <duckhailinux@gmail.com>
2024-10-29 18:51:27 +0700

    Show result after choose anwser by websocket

--- src/components/Quiz/SingleChoice.js
+++ src/components/Quiz/SingleChoice.js
@@ -5,7 +5,9 @@ const SingleChoice = ({
   currentQuestion,
   selectAnswer,
   selectedIndex,
+  selectedResult
 }) => {
+ 
   return (
     <>
       <div className="question-box">
@@ -15,22 +17,47 @@ const SingleChoice = ({
         </div>
       </div>
       <div className="answers-boxes">
-        {question?.choices.map((choice, index) => {
-          return (
-            <label
-              onClick={() => selectAnswer(index)}
-              key={index}
-              htmlFor={index}
-              className={
-                index === selectedIndex[selectedIndex.length -1]
-                  ? "answer-label selected"
-                  : "answer-label"
-              }
-            >
-              {choice.name}
-              <input type="radio" name="answer" id={index} />
-            </label>
-          );
+        {question?.choices.map((choice) => {
+          var index = choice.id
+            if(selectedResult && selectedResult?.correctIndex?.length > 0  ){
+                var result = selectedIndex[0];
+                var correctIndex = selectedResult.correctIndex
+                
+                return (
+                  <label
+                  key={index}
+                  htmlFor={index}
+                  className={
+                   correctIndex.includes(index)
+                      ? "answer-label selected correct"
+                      : index === result && !correctIndex.includes(index)
+                      ? "answer-label selected incorrect"
+                      : "answer-label"
+                  }
+                >
+                  {choice.name}
+                  <input type="radio" name="answer" id={index} />
+                </label>
+                )
+            }else {
+              return (
+                <label
+                onClick={() => selectAnswer(index)}
+                key={index}
+                htmlFor={index}
+                className={
+                  index === selectedIndex[selectedIndex.length -1]
+                    ? "answer-label selected"
+                    : "answer-label"
+                }
+              >
+                {choice.name}
+                <input type="radio" name="answer" id={index} />
+              </label>
+              )
+            }
+           
+          ;
         })}
       </div>
     </>
--- src/services/RecordService.js
+++ src/services/RecordService.js
@@ -2,8 +2,9 @@ import { axiosPrivate } from "../api"
 import { RECORD_URL } from "../utils/Constant"
 
 class RecordService {
-    createRecord(recordItems, quizId, time){
+    createRecord(recordId,recordItems, quizId, time){
         return axiosPrivate.post(RECORD_URL, {
+            recordId: recordId,
             quizId: quizId,
             recordItems: recordItems,
             timeLeft: time,
